b0b8bfda3faae6e6475887ede6fc5a71a8cf17b2
Lorand Mezei <lorand_mezei@yahoo.com>
2021-06-16 13:01:53 -0400

    Evaluator unit test started writing it.

--- Tests/Test_Evaluator.py
+++ Tests/Test_Evaluator.py
@@ -9,11 +9,39 @@ class Test_Evaluator(unittest.TestCase):
     exp6 = ['(', 'x', ')']
     exp7 = ['(', '-', 'x', ')']
 
-    def test_evaluate(self):
-        pass
+    etb = ExpTreeBuilder()
+    ev = Evaluator()
 
-    def test_evaluate_vars(self):
-        pass
+    def test_evaluate_exp1(self):
+        tree = etb.build_exp_tree(self.exp1)
+    def test_evaluate_exp2(self):
+        tree = etb.build_exp_tree(self.exp2)
+    def test_evaluate_exp3(self):
+        tree = etb.build_exp_tree(self.exp3)
+    def test_evaluate_exp4(self):
+        tree = etb.build_exp_tree(self.exp4)
+    def test_evaluate_exp5(self):
+        tree = etb.build_exp_tree(self.exp5)
+    def test_evaluate_exp6(self):
+        tree = etb.build_exp_tree(self.exp6)
+    def test_evaluate_exp7(self):
+        tree = etb.build_exp_tree(self.exp7)
+
+
+    def test_evaluate_vars_exp1(self):
+        tree = etb.build_exp_tree(self.exp1)
+    def test_evaluate_vars_exp2(self):
+        tree = etb.build_exp_tree(self.exp2)
+    def test_evaluate_vars_exp3(self):
+        tree = etb.build_exp_tree(self.exp3)
+    def test_evaluate_vars_exp4(self):
+        tree = etb.build_exp_tree(self.exp4)
+    def test_evaluate_vars_exp5(self):
+        tree = etb.build_exp_tree(self.exp5)
+    def test_evaluate_vars_exp6(self):
+        tree = etb.build_exp_tree(self.exp6)
+    def test_evaluate_vars_exp7(self):
+        tree = etb.build_exp_tree(self.exp7)
 
 if __name__ == '__main__':
     unittest.main()
