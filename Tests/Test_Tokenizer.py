33e1068b5deaea366b0e1bc1218aff15e6745049
Lorand Mezei <lorand_mezei@yahoo.com>
2021-06-16 12:36:40 -0400

    Tokenizer unit test written.

---
+++ Tests/Test_Tokenizer.py
@@ -0,0 +1,34 @@
+import unittest
+
+from Tokenizer import Tokenizer
+
+class Test_Tokenizer(unittest.TestCase):
+    # Expression examples.
+    exp1 = "((cos(x)) * (sin(y)))"
+    exp2 = "((1+2)*(3+4))"
+    exp3 = "(cos(2*x))"
+    exp4 = "(x+((x+5)^2))"
+    exp5 = "(1*(2*3))"
+    exp6 = "(x)"
+    exp7 = "(-x)"
+
+    def test_tokenize_math_exp(self):
+        tk = Tokenizer()
+
+        self.assertEqual(tk.tokenize_math_exp(self.exp1),
+                         ['(', '(', 'cos', '(', 'x', ')', ')', '*', '(', 'sin', '(', 'y', ')', ')', ')'])
+        self.assertEqual(tk.tokenize_math_exp(self.exp2),
+                         ['(', '(', '1', '+', '2', ')', '*', '(', '3', '+', '4', ')', ')'])
+        self.assertEqual(tk.tokenize_math_exp(self.exp3),
+                         ['(', 'cos', '(', '2', '*', 'x', ')', ')'])
+        self.assertEqual(tk.tokenize_math_exp(self.exp4),
+                         ['(', 'x', '+', '(', '(', 'x', '+', '5', ')', '^', '2', ')', ')'])
+        self.assertEqual(tk.tokenize_math_exp(self.exp5),
+                         ['(', '1', '*', '(', '2', '*', '3', ')', ')'])
+        self.assertEqual(tk.tokenize_math_exp(self.exp6),
+                         ['(', 'x', ')'])
+        self.assertEqual(tk.tokenize_math_exp(self.exp7),
+                         ['(', '-', 'x', ')'])
+
+if __name__ == '__main__':
+    unittest.main()
--- Tokenizer.py
+++ Tokenizer.py
@@ -1,17 +1,6 @@
 import re
-import numpy as np
 
 class Tokenizer():
-    def tokenize_math_eq(self, eq):
-        # Split the mathematical equation into two parts: left of equals sign, right of equals sign.
-        tokens = np.array(eq.split('='))  #tokens[0] (left side) and tokens[1] (right side)
-        # Create an array that holds the tokens of the left side.
-        tokens_left = np.array([tokens[0]])
-        # Create an array that holds the tokens of the right side. This will further tokenize the right side expression.
-        tokens_right = np.array(self.tokenize_math_exp(tokens[1]))
-        # Concatenate left and right back together into a full equation.
-        return np.concatenate((tokens_left, ["="], tokens_right))
-
     #https://stackoverflow.com/questions/20805614/tokenize-a-mathematical-expression-in-python, by koffein
     def tokenize_math_exp(self, exp):
         # Tokenize the mathematical expression using a regular expression.
